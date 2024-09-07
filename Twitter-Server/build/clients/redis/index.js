c1df0535086fbfb85420dd23eb779579c51d5ea3
coolmaark <ravikurella1@gmail.com>
2024-09-08 02:19:07 +0530

    redis_1

--- Twitter-Server/build/clients/redis/index.js
+++ Twitter-Server/build/clients/redis/index.js
@@ -3,6 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.redisClient = void 0;
 const redis_1 = require("@upstash/redis");
 exports.redisClient = new redis_1.Redis({
-    url: 'https://relaxed-jennet-57375.upstash.io',
-    token: 'AeAfAAIjcDFlMzM0OWM5MmZmZjM0M2ExOGU5ZDZlOWI0Njg2ODIwZXAxMA',
+    url: process.env.REDIS_URL,
+    token: process.env.REDIS_TOKEN,
 });
--- Twitter-Server/src/clients/redis/index.ts
+++ Twitter-Server/src/clients/redis/index.ts
@@ -1,7 +1,7 @@
 import { Redis } from '@upstash/redis'
 
 export const redisClient = new Redis({
-  url: 'https://relaxed-jennet-57375.upstash.io',
-  token: 'AeAfAAIjcDFlMzM0OWM5MmZmZjM0M2ExOGU5ZDZlOWI0Njg2ODIwZXAxMA',
+  url:  process.env.REDIS_URL,
+  token: process.env.REDIS_TOKEN,
 })
 
